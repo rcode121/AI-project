d784cba5e05f3aa827567d202f3f469a6db1688a
rcode121 <p18riteshkumar@gmail.com>
2023-08-06 16:17:09 +0530

    Login Added

--- mainn.py
+++ mainn.py
@@ -11,13 +11,75 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 import dotenv
 
+import os 
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+
+# Python standard libraries
+import json
+import os
+import sqlite3
+
+# Third party libraries
+from flask import Flask, redirect, request, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+# Internal imports
+from db import init_db_command
+from user import User
+
+# Configuration
+GOOGLE_CLIENT_ID = "569662328285-i4a3tq7v7ipapo5dfj8n0vdlfjaesmld.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "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"
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+
 config = dotenv.dotenv_values(".env")
 openai.api_key = config['OPENAI_API_KEY']
 
 
 
 app = Flask(__name__)
-app.secret_key = 'your secret key'  # Replace with your secret key
+#app.secret_key = 'your secret key'  # Replace with your secret key
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+
+
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return "You must be logged in to access this content.", 403
+
+
+# Naive database setup
+try:
+    init_db_command()
+except sqlite3.OperationalError:
+    # Assume it's already been created
+    pass
+# OAuth2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
 
 conversation_chains = {}
 
@@ -55,6 +117,10 @@ def get_conversation_chain(vectorstore):
         memory=memory
     )
     return conversation_chain
+
+
+
+
 
 @app.route('/api/chat_without_file', methods=['POST'])
 def chat_without_file():
@@ -119,16 +185,121 @@ def chat_with_file():
     os.remove(filename)
     return jsonify(message = response['answer'])
 
+
+@app.route("/")
+def index():
+    if current_user.is_authenticated:
+        return render_template('index.html')
+    else:
+        return render_template('login.html')
     
-@app.route('/')
-def home():
-    return render_template('index.html')
+    
+
 @app.route("/style.css")
 def styles():
     return send_from_directory("static", "style.css")    
 
+
+@app.route("/login")
+def login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code,
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that we have tokens (yay) let's find and hit URL
+    # from Google that gives you user's profile information,
+    # including their Google Profile Image and Email
+
+
+
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+
+
+    userinfo_endpoint = "https://www.googleapis.com/oauth2/v3/userinfo"
+
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+    for i in userinfo_response:
+        print(i)
+    # We want to make sure their email is verified.
+    # The user authenticated with Google, authorized our
+    # app, and now we've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    # Create a user in our db with the information provided
+    # by Google
+    user = User(
+        id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+    )
+
+    # Doesn't exist? Add to database
+    if not User.get(unique_id):
+        User.create(unique_id, users_name, users_email, picture)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect(url_for("index"))
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True, port=5000)
+
+
+
 
 
 
